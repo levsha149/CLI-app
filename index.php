abce202c7d052ec2e932640344aafe00a361cdcb
Alexey <alexey.solomka@techs.com.ua>
2020-10-05 11:52:00 +0300

    backup

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
-
 require __DIR__ . '/src/autoload.php';
+
+use Classes\TestClass;
+
+$obj = new TestClass();
--- src/autoload.php
+++ src/autoload.php
@@ -2,5 +2,14 @@
 require_once __DIR__ . '/helpers/Autoloader.php';
 
 $autoloader = new Autoloader();
+
+/**
+ * register namespaces here
+ */
 $autoloader->registerNamespace('Classes',  __DIR__ . '/classes');
+
+
+/**
+ * register function call
+ */
 $autoloader->register();
